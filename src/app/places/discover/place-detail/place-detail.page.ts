8cf79de388a123344455a712e194158504b00680
Stellce <andrey.is323@gmail.com>
2024-02-14 16:24:49 +0100

    Navigation between places and offers

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {NavController} from "@ionic/angular";
+import {Place} from "../../place.model";
+import {PlacesService} from "../../places.service";
 
 @Component({
   selector: 'app-place-detail',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
-
-  constructor() { }
+  place: Place;
+  constructor(
+    private route: ActivatedRoute,
+    private navCtrl: NavController,
+    private placesService: PlacesService
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if(!paramMap.has('placeId')) return this.navCtrl.navigateBack('/places/offers');
+      this.place = this.placesService.getPlace( paramMap.get('placeId'));
+    })
+  }
+
+  onBookPlace() {
+    // this.router.navigateByUrl('/places/discover');
+    this.navCtrl.navigateBack('/places/discover');
   }
 
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -6,13 +6,25 @@ import {Place} from "./place.model";
 })
 export class PlacesService {
   private _places: Place[] = [
-    new Place('p1', 'Manhattan Mansion', 'In the heart of New York City', 'https://upload.wikimedia.org/wikipedia/commons/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg', 149),
-    new Place('p1', 'Manhattan Mansion', 'In the heart of New York City', 'https://upload.wikimedia.org/wikipedia/commons/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg', 149),
-    new Place('p1', 'Manhattan Mansion', 'In the heart of New York City', 'https://upload.wikimedia.org/wikipedia/commons/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg', 149),
+    new Place('p1', 'Manhattan Mansion', 'In the heart of New York City', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg/288px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg', 149),
+    new Place('p1', 'Manhattan Mansion', 'In the heart of New York City', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg/288px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg', 149),
+    new Place('p1', 'Manhattan Mansion', 'In the heart of New York City', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg/288px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg', 149)
   ];
+  private _offers: Place[] = [
+    new Place('p1', 'Manhattan Mansion', 'In the heart of New York City', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg/288px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg', 149),
+    new Place('p1', 'Manhattan Mansion', 'In the heart of New York City', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg/288px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg', 149),
+    new Place('p1', 'Manhattan Mansion', 'In the heart of New York City', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg/288px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg', 149)
+  ];
+  constructor() {}
   get places() {
     return [...this._places];
   }
 
-  constructor() { }
+  get offers() {
+    return [...this._offers];
+  }
+
+  getPlace(id: string) {
+    return {...this._places.find(p => p.id === id)}
+  }
 }
