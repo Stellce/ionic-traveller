b716866eaa4815695aa85eb5222e18a2e2ad55d0
Stellce <andrey.is323@gmail.com>
2024-02-25 03:16:57 +0100

    Added the Edit Offer form

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute} from "@angular/router";
 import {NavController} from "@ionic/angular";
 import {Place} from "../../place.model";
 import {PlacesService} from "../../places.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-offer',
@@ -11,6 +12,7 @@ import {PlacesService} from "../../places.service";
 })
 export class EditOfferPage implements OnInit {
   place: Place;
+  form: FormGroup;
   constructor(
     private route: ActivatedRoute,
     private navCtrl: NavController,
@@ -21,7 +23,22 @@ export class EditOfferPage implements OnInit {
     this.route.paramMap.subscribe(paramMap => {
       if(!paramMap.has('placeId')) return this.navCtrl.navigateBack('/places/offers');
       this.place = this.placesService.getPlace( paramMap.get('placeId'));
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        }),
+      })
     })
+  }
+
+  onUpdateOffer() {
+    if(this.form.invalid) return;
+    console.log(this.form);
   }
 
 }
