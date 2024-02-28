3fe1904c8a0ef93a73f2334e138f4ee540a4a087
Stellce <andrey.is323@gmail.com>
2024-02-28 16:14:43 +0100

    Cancelling Bookings added

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -39,10 +39,20 @@ export class BookingService {
     );
     return this.bookings.pipe(
       take(1),
+      delay(1000),
       tap(bookings => {
         this._bookings.next(bookings.concat(newBooking));
-      }),
-      delay(1000)
+      })
+    );
+  }
+
+  cancelBooking(bookingId: string) {
+    return this.bookings.pipe(
+      take(1),
+      delay(1000),
+      tap(bookings => {
+        this._bookings.next(bookings.filter(b => b.id !== bookingId));
+      })
     );
   }
 
--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {BookingService} from "./booking.service";
 import {Booking} from "./booking.model";
-import {IonItemSliding} from "@ionic/angular";
+import {IonItemSliding, LoadingController} from "@ionic/angular";
 import {Subscription} from "rxjs";
 
 @Component({
@@ -12,7 +12,10 @@ import {Subscription} from "rxjs";
 export class BookingsPage implements OnInit, OnDestroy {
   loadedBookings: Booking[];
   private bookingsSub: Subscription;
-  constructor(private bookingsService: BookingService) {}
+  constructor(
+    private bookingsService: BookingService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {
     this.bookingsSub = this.bookingsService.bookings.subscribe(bookings =>
@@ -21,6 +24,11 @@ export class BookingsPage implements OnInit, OnDestroy {
 
   onCancelBooking(bookingId: string, slidingEl: IonItemSliding) {
     slidingEl.close();
+    this.loadingCtrl.create({message: 'Cancelling...'}).then(loadingEl => {
+      loadingEl.present;
+      this.bookingsService.cancelBooking(bookingId).subscribe(() =>
+        loadingEl.dismiss());
+    })
   }
 
   ngOnDestroy() {
