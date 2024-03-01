7735e561c8c2605c1ebcb344e0c7eac9ba999ed0
Stellce <andrey.is323@gmail.com>
2024-03-01 23:23:07 +0100

    Added script for map loading

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {ModalController} from "@ionic/angular";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss'],
 })
-export class MapModalComponent  implements OnInit {
+export class MapModalComponent  implements OnInit, AfterViewInit {
+  private apiKey = environment.apiKey;
 
   constructor(
     private modalCtr: ModalController
@@ -14,8 +16,39 @@ export class MapModalComponent  implements OnInit {
 
   ngOnInit() {}
 
+  ngAfterViewInit() {
+    this.getGoogleMaps().then(googleMaps => {
+
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
   onCancel() {
    this.modalCtr.dismiss();
+  }
+
+  private getGoogleMaps(): Promise<any> {
+    const win = window as any;
+    const googleModule = win.google;
+    if(googleModule && googleModule.maps) {
+      return Promise.resolve(googleModule.maps);
+    }
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = `"https://maps.googleapis.com/maps/api/js?key=${this.apiKey}&loading=async&callback=initMap"`
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      script.onload = () => {
+        const loadedGoogleModule = win.google;
+        if (loadedGoogleModule && loadedGoogleModule.maps) {
+          resolve(loadedGoogleModule.maps);
+        } else {
+          reject('Google maps SDK not available');
+        }
+      }
+    })
   }
 
 }
