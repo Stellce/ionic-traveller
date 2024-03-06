191760e142815c1c8c9df45f49fd982350a1eb12
Stellce <andrey.is323@gmail.com>
2024-03-06 08:54:53 +0100

    Fetching a static image url

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -3,7 +3,8 @@ import {ModalController} from "@ionic/angular";
 import {MapModalComponent} from "../../map-modal/map-modal.component";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../../environments/environment";
-import {map} from "rxjs";
+import {map, of, switchMap} from "rxjs";
+import {PlaceLocation} from "../../../places/location.model";
 
 @Component({
   selector: 'app-location-picker',
@@ -23,16 +24,44 @@ export class LocationPickerComponent  implements OnInit {
     this.modalCtrl.create({ component: MapModalComponent }).then(modalEl => {
       modalEl.onDidDismiss().then(modalData => {
         if(!modalData?.data) return;
-        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(() => {});
+        const pickedLocation: PlaceLocation = {
+          lat: modalData.data.lat,
+          lng: modalData.data.lng,
+          address: null,
+          staticMapImageUrl: null
+        }
+        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(switchMap(address => {
+          pickedLocation.address = address;
+          return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
+        })).subscribe(staticMapImageUrl => {
+          pickedLocation.staticMapImageUrl = staticMapImageUrl;
+        });
       });
       modalEl.present();
     });
   }
 
   private getAddress(lat: number, lng: number) {
-    return this.http.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&${this.apiKey}`).pipe(map(geoData => {
-      console.log(geoData);
-    }))
+    return this.http
+      .get<any>(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${
+          environment.apiKey
+        }`
+      )
+      .pipe(
+        map(geoData => {
+          if (!geoData || !geoData.results || geoData.results.length === 0) {
+            return null;
+          }
+          return geoData.results[0].formatted_address;
+        })
+      );
+  }
+
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap
+    &markers=color:red%7Clabel:Place%7C${lat},${lng}
+    &key=${environment.apiKey}`;
   }
 
 }
